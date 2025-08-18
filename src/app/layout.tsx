9e8a876b7089c6a3ef0d8edbac07efaa80e3d85d
Tran Quoc <tranquoc1301@gmail.com>
2025-08-18 16:12:00 +0700

    Change font to Chirp Twitter Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,17 @@ import "./globals.css";
 import LeftBar from "@/components/LeftBar";
 import RightBar from "@/components/RightBar";
 import { ImageKitProvider } from "@imagekit/next";
+import localFont from "next/font/local";
+
+const chirp = localFont({
+  src: [
+    { path: "../../public/fonts/chirp-regular-web.woff", weight: "400" },
+    { path: "../../public/fonts/chirp-medium-web.woff", weight: "500" },
+    { path: "../../public/fonts/chirp-bold-web.woff", weight: "700" },
+  ],
+  variable: "--font-chirp",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -9,10 +20,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={chirp.variable}>
+      <head></head>
       <body>
         <ImageKitProvider urlEndpoint={process.env.NEXT_PUBLIC_URL_ENDPOINT}>
-          <main>
+          <main className="font-family-chirp">
             <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
               <section
                 aria-label="Left Sidebar"
